5346b1b220cb5625ca6b0195b96797e8681dfbef
tomibenitez <tomibenitez@users.noreply.github.com>
2021-03-10 17:16:12 +0000

    deploy: c43da31d1cc254089d5750b6a6f1c40bcc267284

--- src/handlebars/helpers/blocks/btn.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (href, context) {
-    let classes = context.hash.class ? context.hash.class : '';
-    let tag = href ? 'a' : 'button';
-    href = href ? `href="${href}"` : '';
-
-    return (
-        `<${tag} class="c-btn ${classes}" ${href}>${context.fn(this)}</${tag}>`
-    );
-}
--- src/handlebars/helpers/path.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (path, context) {
-
-    return path;
-}
